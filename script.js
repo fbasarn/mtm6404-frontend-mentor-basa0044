6045660efa1710f0492fab660f8a47394673ae1a
Feyza Basaran <basa0044@algonquinlive.com>
2023-03-08 17:04:21 -0500

    dropdown buttons display on click functionality

---
+++ script.js
@@ -0,0 +1,26 @@
+const dropdown = document.querySelector('.click')
+const arrup1 = document.querySelector('.arrow-up1')
+const arrdown1 = document.querySelector('.arrow-down1')
+const arrup2 = document.querySelector('.arrow-up2')
+const arrdown2 = document.querySelector('.arrow-down2')
+const dropdown2 = document.querySelector('.click2')
+const ddcontent = document.querySelector('.dropdown-content')
+const ddcontent2 = document.querySelector('.dropdown-content2')
+
+
+dropdown.addEventListener('click', function(e){
+    arrup1.classList.toggle('display')
+    arrdown1.classList.toggle('non-display')
+    ddcontent.classList.toggle('display')
+})
+
+dropdown2.addEventListener('click', function(){
+    arrup2.classList.toggle('display')
+    arrdown2.classList.toggle('non-display')
+    ddcontent2.classList.toggle('display')   
+})
+
+
+
+
+
