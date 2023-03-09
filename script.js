6c9257dd35c4aa2377db68e77a88110b3816d56e
Feyza Basaran <basa0044@algonquinlive.com>
2023-03-09 16:45:43 -0500

    hamburger nav stlinging in mobile screens

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-// const $container = document.querySelector('.container')
-// const $openMenu = document.querySelector('.hamburger-icon')
-// const $closeMenu = document.querySelector('.cross-icon')
-// const $nav = document.querySelector('.nav-items')
+const $container = document.querySelector('.container')
+const $openMenu = document.querySelector('.hamburger-icon')
+const $closeMenu = document.querySelector('.cross-icon')
+const $nav = document.querySelector('nav')
 
+$openMenu.addEventListener('click', function ShowMenu(){
+    $nav.style.display = 'block'
+    $closeMenu.style.display = 'block'
+    $openMenu.style.display = 'none'
+})
 
-
-// $openMenu.addEventListener('click', function ShowMenu(){
-//     $nav.style.display = 'flex'
-//     $closeMenu.style.display = 'flex'
-//     $openMenu.style.display = 'none'
-// })
-
-// $closeMenu.addEventListener('click', function CloseMenu(){
-//     $nav.style.display = 'none'
-//     $closeMenu.style.display = 'none'
-//     $openMenu.style.display = 'flex'
-// } )
+$closeMenu.addEventListener('click', function CloseMenu(){
+    $nav.style.display = 'none'
+    $closeMenu.style.display = 'none'
+    $openMenu.style.display = 'block'
+} )
 
 
 
