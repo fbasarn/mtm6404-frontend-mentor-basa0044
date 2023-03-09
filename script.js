7b1bff54f4a1e302240409f62f75d3da7618dc1e
Feyza Basaran <basa0044@algonquinlive.com>
2023-03-09 13:26:05 -0500

    changing html/css for hamburger navigation

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+const $container = document.querySelector('.container')
+const $openMenu = document.querySelector('.hamburger-icon')
+const $closeMenu = document.querySelector('.cross-icon')
+const $nav = document.querySelector('.nav-items')
 
+
+
+$openMenu.addEventListener('click', function ShowMenu(){
+    $nav.style.display = 'flex'
+    $closeMenu.style.display = 'flex'
+    $openMenu.style.display = 'none'
+})
+
+$closeMenu.addEventListener('click', function CloseMenu(){
+    $nav.style.display = 'none'
+    $closeMenu.style.display = 'none'
+    $openMenu.style.display = 'flex'
+} )
+
+
+
+    
 
 
 
